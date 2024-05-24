a451969d389e57b9d6e0065644757d6fc1397a1e
Vbhanusai <bhanusaiveeramallu@gmail.com>
2024-05-24 22:55:48 +0530

    fixed redirecting html page link3

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+app.static_folder = "static/"
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/upload')
-def index():
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
     return render_template('upload.html')
 
 if __name__ == '__main__':
