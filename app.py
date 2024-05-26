7d53f0c460b0ce11e6d5fe05921e00e1ce2bdd52
Vbhanusai <bhanusaiveeramallu@gmail.com>
2024-05-26 18:23:25 +0530

    Image upload and saving

--- app.py
+++ app.py
@@ -1,14 +1,67 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+# from predict_disease import prediction_disease_type
+import cv2
 
 app = Flask(__name__)
 app.static_folder = "static/"
+
+
+# Home route
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
+# Upload route
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
+    
+    if request.method == 'POST':
+        
+        # Assuming you have an image file uploaded
+        plant_type = request.form['plant_type']
+        file = request.files['image']
+
+        if file.filename == '':
+            return 'No selected file', 400
+        
+        # Save the file to a temporary location
+        file_path = "Uploads/"+file.filename
+        file.save(file_path)
+
+        img = cv2.imread(file_path)
+        print("image path",file_path)
+        print("image shape",img.shape)
+
+        disease="Rust"
+        prob = 91
+        # Assuming the file name is in the format "plant_disease.jpg"
+        # You may need to adjust this depending on your naming convention
+
+        # print()
+        # Fetch corresponding remedy and product image from MongoDB
+        remedy = "kkkkk"
+
+        # Redirect to the result page, passing necessary data
+        # return redirect(url_for('result', predicted_disease=predicted_disease, remedy=remedy))
+        return render_template('result.html', predicted_disease=disease, remedy=remedy)
+
     return render_template('upload.html')
+
+# Result route
+@app.route('/result')
+def result():
+    # Get data passed from the upload route
+    predicted_disease = request.args.get('predicted_disease')
+    # remedy = request.args.get('remedy')
+    remedy = "spray"
+    # Example print statement
+    print(f"Predicted Disease: {predicted_disease}, Remedy: {remedy}")
+
+    return render_template('result.html', predicted_disease=predicted_disease, remedy=remedy)
+
+# Add any additional routes and logic as needed
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
