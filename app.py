42e8454599fd632e9b49293d18e4b0d318284fb7
Vbhanusai <bhanusaiveeramallu@gmail.com>
2024-06-04 11:36:32 +0530

    small change on app.py import

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 # from predict_disease import prediction_disease_type
 # import cv2
-# import numpy as np
+import numpy as np
 # import io
 
 app = Flask(__name__)
